9eea008662b6d87f0d25ba1640f2f64f5d31e5a5
lxrNovember <3489916262@qq.com>
2026-03-09 16:21:31 +0800

    Fix: Swagger configuration

---
+++ src/main/java/com/example/usermanagement/config/WebMvcConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.usermanagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
